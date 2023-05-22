916623460cab899f640444eccd40a5125517ebaf
DESKTOP-HDSE6EU\KGA LoL <a>
2023-05-22 14:43:01 +0900

    로거 클래스 추가

---
+++ SuperMarioMaker/Assets/02.Scripts/New Folder/Logger.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public static class Logger
+{
+    [Conditional("EnableLogger")]
+    public static void Debug(object logMsg)
+    {
+        UnityEngine.Debug.Log(logMsg);
+    }
+
+    [Conditional("EnableLogger")]
+    public static void Debug(object message, Object context)
+    {
+        UnityEngine.Debug.Log(message, context);
+    }
+
+    [Conditional("EnableLogger")]
+    public static void Error(object message)
+    {
+        UnityEngine.Debug.LogError(message);
+    }
+
+    [Conditional("EnableLogger")]
+    public static void Error(object message, Object context)
+    {
+        UnityEngine.Debug.LogError(message, context);        
+    }
+}
