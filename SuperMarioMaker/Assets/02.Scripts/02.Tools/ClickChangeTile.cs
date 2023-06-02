a6e6e710cff520c8728664d0855e3df63537b6bb
DESKTOP-UM7NU9E\kshan <kshan9279@gmail.com>
2023-06-02 16:33:51 +0900

    new input system tool 에 기능 추가

--- SuperMarioMaker/Assets/02.Scripts/02.Tools/ClickChangeTile.cs
+++ SuperMarioMaker/Assets/02.Scripts/02.Tools/ClickChangeTile.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 using static UnityEditor.PlayerSettings;
 
@@ -27,6 +28,46 @@ public class ClickChangeTile : MonoBehaviour
         }
     }
 
+    public void TestFunc(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Logger.Debug("©Л");
+        }
+
+
+
+    }
+    public void AwTestFunc(InputAction.CallbackContext context)
+    {
+
+        var dir = context.ReadValue<float>();
+
+        //if (context.started)
+        //{
+        //    if (dir > 0)
+        //    {
+        //        Logger.Debug("©Л");
+        //    }
+        //    else
+        //    {
+        //        Logger.Debug("аб");
+
+        //    }
+        //}
+        //if (context.performed)
+        {
+            if (context.ReadValue<float>() > 0)
+            {
+                Logger.Debug("©Л");
+            }
+            else if(context.ReadValue<float>() < 0)
+            {
+                Logger.Debug("аб");
+
+            }
+        }
+    }
     void ChangeTileUnderMouse()
     {
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
