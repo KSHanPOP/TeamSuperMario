301f840d047610b671deb984799e2c5ef8245b9c
DESKTOP-2CR6NRG\KGA PUBG <oos1928@gmail.com>
2023-06-05 15:51:16 +0800

    오브젝트 생성용 커스텀 타일 스크립트 추가

---
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/Tile/ObjectTile.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+public class ObjectTile : CustomTile
+{
+    [SerializeField]
+    private GameObject prefap;
+
+    protected virtual void InstancePrefab()
+    {
+        
+    }
+}
