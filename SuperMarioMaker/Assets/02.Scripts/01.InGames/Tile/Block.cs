4e28d2cc8b9263a1961eff0e6a0666b576bf3fa4
gh1928 <oos1928@gmail.com>
2023-06-19 00:45:08 +0900

    블록 흔들림 효과 추가

--- SuperMarioMaker/Assets/02.Scripts/01.InGames/Tile/Block.cs
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/Tile/Block.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 public class Block : MonoBehaviour
 {
@@ -11,13 +13,25 @@ public class Block : MonoBehaviour
     private EnumItems itemType;
 
     [SerializeField]
-    private int coinCount;    
+    private int coinCount;
 
+    [SerializeField]
+    private float shakeTime;
+
+    [SerializeField]
+    private float maxShakeHeight;
+
+    private bool isHitable = true;
+    
     private SpriteRenderer spriteRenderer;
+
+    private Transform spriteTransform;
+
 
     private void Awake()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteTransform = spriteRenderer.transform;
         SetStartSprite();
     }
     private void SetStartSprite()
@@ -27,10 +41,43 @@ public class Block : MonoBehaviour
     }
     public void SmallHit()
     {
-        Logger.Debug("smallHIt");
+        if (!isHitable)
+            return;
+        
+        StartCoroutine(ShakeCoroutine());
     }
     public void BigHit()
     {
+        if (!isHitable)
+            return;
+
         Logger.Debug("bigHIt");
+    }
+
+    IEnumerator ShakeCoroutine()
+    {
+        isHitable = false;
+
+        float timer = 0f;
+        float height;
+        float divShakeTime = 1 / shakeTime;        
+
+        Vector3 newPos = Vector3.zero;
+
+        while(timer < shakeTime)
+        {
+            timer += Time.deltaTime;                       
+            height = Mathf.Lerp(0f, maxShakeHeight, Mathf.Sin(timer * divShakeTime * Mathf.PI));
+            
+            newPos.y = height;
+
+            spriteTransform.localPosition = newPos;           
+
+            yield return null;
+        }
+
+        spriteTransform.localPosition = Vector3.zero;
+        isHitable = true;
+        yield break;
     }
 }
