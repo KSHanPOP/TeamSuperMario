e34b1df2b9177bdd5211c6b88b1a3f09e4ff2444
KI2022061401\USER <a>
2023-06-21 18:56:44 +0900

    미스터리 블록 기능 완료

--- SuperMarioMaker/Assets/02.Scripts/01.InGames/Tile/QuestionBlock.cs
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/Tile/QuestionBlock.cs
@@ -1,11 +1,25 @@
+using UnityEngine;
 
 public class QuestionBlock : Block
 {
+    private int hashUsed = Animator.StringToHash("Used");
+    private Animator animator;
+    
     protected override void Awake()
     {
+        animator = GetComponentInChildren<Animator>();
+
         if (itemType == EnumItems.None)
             itemType = EnumItems.Coin;
 
         base.Awake();
+    }
+
+    protected override void CheckItemRemainCount()
+    {
+        --itemCount;
+
+        if (itemCount == 0)
+            animator.SetTrigger(hashUsed);            
     }
 }
