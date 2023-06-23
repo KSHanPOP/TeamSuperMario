1aed37c9c9455e064e35450e9dcad7aebc6ff0f0
DESKTOP-UM7NU9E\kshan <kshan9279@gmail.com>
2023-06-23 21:27:31 +0900

    프리펩 인포 클라스 추가

---
+++ SuperMarioMaker/Assets/02.Scripts/02.Tools/PrefapInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefapInfo : MonoBehaviour
+{
+    [SerializeField] String iconSpritePath;
+    public String IconSpritePath
+    {
+        get { return iconSpritePath; }
+    }
+
+}
