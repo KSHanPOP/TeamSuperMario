eef586f81c638387f36a7a57a513f48b4f58ee7e
gh1928 <oos1928@gmail.com>
2023-06-29 19:33:34 +0900

    이동발판 테스트 코드 작성

---
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/UtilObject/MovingPlatform.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField]
+    private float moveTime = 2f;
+
+    private float timer = 0f;    
+
+    private float dir = 1;
+
+    private bool isWork = true;
+
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            isWork = !isWork;
+        }
+
+        if (!isWork)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
+        timer += Time.deltaTime;
+
+        if(timer > moveTime)
+        {
+            timer = 0f;
+
+            dir = -dir;
+        }
+
+        rb.velocity = new Vector2(0, dir * speed);
+    }
+
+}
