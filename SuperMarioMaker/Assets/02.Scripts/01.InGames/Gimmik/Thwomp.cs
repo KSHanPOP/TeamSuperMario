1830a60127555a08d0eb7486c38fbd8cef73b43f
gh1928 <oos1928@gmail.com>
2023-07-03 23:09:21 +0900

    기믹>쿵쿵 이미지, 애니메이션 기본 작업

---
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/Gimmik/Thwomp.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thwomp : MonoBehaviour
+{
+    private readonly int hashStartFall = Animator.StringToHash("StartFall");
+    private readonly int hashBackToPosition = Animator.StringToHash("BackToPosition");
+    private readonly int hashStartIdle = Animator.StringToHash("StartIdle");
+
+    private Transform player;
+
+
+}
---
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/Monster/Plant/SmallPlant.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallPlant : MonoBehaviour, IShakeable
+{
+    public void Shake(Vector2 _)
+    {
+        Logger.Debug("plant shaked");
+        Destroy(gameObject, 0.1f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<CollisionMonster>().Hit();
+        }
+    }
+}
