7a5e681fe708ebf42437399c0bfbfcfe8f25e95a
gh1928 <oos1928@gmail.com>
2023-07-17 14:21:42 +0900

    카메라 팔로워 스크립트 변경

--- SuperMarioMaker/Assets/02.Scripts/01.InGames/CameraFollowPlayer.cs
+++ SuperMarioMaker/Assets/02.Scripts/01.InGames/CameraFollowPlayer.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public Transform player;
     public float smoothTime = 0.3F;
     private Vector3 velocity = Vector3.zero;
 
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
+    private Camera cam;
+
+    private float minX;
+    private float maxX;
+    private float minY;
+    private float maxY;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
 
     void Update()
     {
-        if (player != null)
-        {
-            // Set the boundaries
-            minX = 0f + ToolManager.Instance.TilemapX / 2;
-            maxX = ToolManager.Instance.TilemapX * ToolManager.Instance.MapRowLength - minX;
-            minY = 0f + ToolManager.Instance.TilemapY / 2;
-            maxY = ToolManager.Instance.TilemapY * ToolManager.Instance.MapColLength - minY;
+        minX = 0f + ToolManager.Instance.TilemapX / 2;
+        maxX = ToolManager.Instance.TilemapX * ToolManager.Instance.MapRowLength - minX;
+        minY = 0f + ToolManager.Instance.TilemapY / 2;
+        maxY = ToolManager.Instance.TilemapY * ToolManager.Instance.MapColLength - minY;
 
-            Vector3 targetPosition = player.TransformPoint(new Vector3(0, 0, -10));
-            Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-            smoothPosition.x = Mathf.Clamp(smoothPosition.x, minX, maxX);
-            smoothPosition.y = Mathf.Clamp(smoothPosition.y, minY, maxY);
-            transform.position = smoothPosition;
-        }
+        Vector3 targetPosition = transform.TransformPoint(new Vector3(0, 0, -10));
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        smoothPosition.x = Mathf.Clamp(smoothPosition.x, minX, maxX);
+        smoothPosition.y = Mathf.Clamp(smoothPosition.y, minY, maxY);
+        cam.transform.position = smoothPosition;
     }
 }
