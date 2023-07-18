87bdc3edebe70c11cd165d3a352e7461e380849e
pointerNreference <kshan9279@gmail.com>
2023-07-19 06:55:05 +0900

    제이슨 로드 완

--- SuperMarioMaker/Assets/02.Scripts/00.Manager/SaveLoadManager.cs
+++ SuperMarioMaker/Assets/02.Scripts/00.Manager/SaveLoadManager.cs
@@ -1,10 +1,15 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveLoadManager : MonoBehaviour
 {
     public static SaveLoadManager Instance { get; private set; }
+
+    List<MapData> mapDatas= new List<MapData>();
+
     void Awake()
     {
         if (Instance == null)
@@ -16,6 +21,36 @@ public class SaveLoadManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        LoadAllMapData();
+    }
+
+    public List<MapData> LoadAllMapData()
+    {
+        string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "SaveData");
+        var mapDataList = new List<MapData>();
+
+        // Check if the directory exists
+        if (Directory.Exists(folderPath))
+        {
+            // Get all .json files in the directory
+            string[] files = Directory.GetFiles(folderPath, "*.json");
+
+            // Loop through each file
+            foreach (string file in files)
+            {
+                // Read the file content
+                string json = File.ReadAllText(file);
+
+                // Deserialize the JSON string to a MapData object
+                MapData mapData = JsonConvert.DeserializeObject<MapData>(json);
+
+                // Add the MapData object to the list
+                mapDataList.Add(mapData);
+            }
+        }
+
+        return mapDataList;
     }
 
     // Start is called before the first frame update
