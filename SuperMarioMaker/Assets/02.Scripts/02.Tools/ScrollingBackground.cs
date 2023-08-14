8be239f4d4e0ad45a7717cb50889cb935336d4e5
gh1928 <oos1928@gmail.com>
2023-08-14 12:16:10 +0900

    노드 방문 시 최단거리 먼저 방문하도록 설정

--- SuperMarioMaker/Assets/02.Scripts/02.Tools/ScrollingBackground.cs
+++ SuperMarioMaker/Assets/02.Scripts/02.Tools/ScrollingBackground.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class ScrollingBackground : MonoBehaviour
